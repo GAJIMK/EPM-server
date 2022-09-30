d6c1e135667e191a96ccc5954c989b662210dfa4
jihye_son <jiiihyeee98@gmail.com>
2022-09-30 15:54:58 +0900

    user accountId 로 info get api 구현

--- src/main/java/com/dartmedia/dmss/controller/AuthController.java
+++ src/main/java/com/dartmedia/dmss/controller/AuthController.java
@@ -1,17 +1,22 @@
 package com.dartmedia.dmss.controller;
 
+import java.util.List;
+
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.dartmedia.dmss.common.CommonResult;
+import com.dartmedia.dmss.common.MultiResult;
 import com.dartmedia.dmss.core.ResponseService;
 import com.dartmedia.dmss.core.ResponseService.CommonResponse;
+import com.dartmedia.dmss.dto.Account;
 import com.dartmedia.dmss.dto.Auth;
 import com.dartmedia.dmss.service.AuthService;
 
@@ -52,4 +57,22 @@ public class AuthController {
 
     return ResponseEntity.ok().body(rs);
   }
+  @ApiOperation(value = "게시글 개별 조회", notes = "게시글 개별 조회 ")
+  @GetMapping("/findById")
+  public ResponseEntity<?> findById(String accountId) {
+    MultiResult<Account> result = null;
+
+    try {
+      List<Account> list = service.findById(accountId);
+      if (list.size() > 0)
+        result = responseService.getMultiResult(list);
+      else
+        result = responseService.getMultiFailType(CommonResponse.NODATA);
+    } catch (Exception e) {
+      log.error("예외:" + e.getMessage());
+      result = responseService.getMultiFailType(ResponseService.CommonResponse.ERR);
+    }
+    return ResponseEntity.ok().body(result);
+  }
+
 }
